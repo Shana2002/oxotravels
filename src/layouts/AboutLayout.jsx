c23483c3b0faf506f6e66cea3cfa7232cfe2f300
Shana2002 <hansakaravi02@gmail.com>
2025-05-27 06:57:10 +0530

    about basic responsive

--- src/layouts/AboutLayout.jsx
+++ src/layouts/AboutLayout.jsx
@@ -4,7 +4,7 @@ import IconCard from "@/components/IconCard";
 
 const AboutLayout = () => {
   return (
-    <div className="w-full h-[90vh] flex flex-col items-center">
+    <div className="w-full h-[180vh] lg:h-[90vh] flex flex-col items-center">
       <motion.h1
         whileInView={{ opacity: 1, y: 0 }}
         initial={{ opacity: 0, y: -100 }}
@@ -13,12 +13,12 @@ const AboutLayout = () => {
       >
         Explore the infinity
       </motion.h1>
-      <div className="w-full h-full px-[2vw] flex">
-        <div className="w-7/12 relative">
+      <div className="w-full h-full px-[2vw] flex flex-col lg:flex-row">
+        <div className="w-full lg:w-7/12 relative">
           <img
             src="/image1.png"
             alt=""
-            className="w-full h-full object-cover absolute left-0 top-0"
+            className="w-full h-full object-cover absolute left-0 top-0 "
           />
           <div className="absolute top-0 left-0 w-full h-full">
             <motion.h2
@@ -47,7 +47,7 @@ const AboutLayout = () => {
               whileInView={{ opacity: 1, x: 0 }}
               initial={{ opacity: 0, x: -100 }}
               transition={{ duration: 0.5 }}
-              className="w-10/12 text-justify mb-2"
+              className="w-10/12 text-justify mb-2 mx-auto lg:mx-0"
             >
               Sri Lanka is a tropical island nation in South Asia, known for its
               stunning beaches, ancient temples, rich wildlife, and vibrant
@@ -59,7 +59,7 @@ const AboutLayout = () => {
               whileInView={{ opacity: 1, x: 0 }}
               initial={{ opacity: 0, x: -100 }}
               transition={{ duration: 0.5 }}
-              className="w-10/12 text-justify mb-2"
+              className="w-10/12 text-justify mb-2 mx-auto lg:mx-0"
             >
               A land of endless beauty and vibrant culture, Sri Lanka offers
               unforgettable experiences for every kind of traveler:
@@ -68,7 +68,7 @@ const AboutLayout = () => {
               whileInView={{ opacity: 1, x: 0 }}
               initial={{ opacity: 0, x: -100 }}
               transition={{ duration: 0.5 }}
-             className="flex w-11/12 mt-auto justify-around ">
+             className="flex w-11/12 mt-auto justify-around mx-auto flex-wrap gap-2">
               <IconCard text={"Relax on Golden Beaches"} icon={"/1.png"}/>
               <IconCard text={"Relax on Golden Beaches"} icon={"/1.png"}/>
               <IconCard text={"Relax on Golden Beaches"} icon={"/1.png"}/>
@@ -77,7 +77,7 @@ const AboutLayout = () => {
             </motion.div>
           </div>
         </div>
-        <div className="w-5/12 relative">
+        <div className="w-full bg-black h-1/2 lg:w-5/12 relative">
           <motion.img
             whileInView={{opacity:1,right:1}}
             initial={{opacity:0,right:-100}}
